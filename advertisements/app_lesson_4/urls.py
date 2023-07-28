d2cf9612e53fae7bdc3869e60cf5f7e924d214dc
Григорий <yurchenko.grigoriy@list.ru>
2023-07-28 17:49:23 +0500

    дз по 5 занятию

--- advertisements/app_lesson_4/urls.py
+++ advertisements/app_lesson_4/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 
 urlpatterns = [
-    path('lesson_4/', index)
+    path('index/', index, name='main-page'),
+
+    path('top-sellers/', top_sellers, name='top-sellers')
 ]
--- advertisements/app_lesson_4/views.py
+++ advertisements/app_lesson_4/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Домашка по 4 занятию')
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
 
 # Create your views here.
